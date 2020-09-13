c7e00c8f877b6cebb5e922b1c8178e852a7705fb
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2020-09-12 20:34:25 -0700

    3.0.1

--- setup.py
+++ setup.py
@@ -20,7 +20,10 @@ VERSION = "3.0.1"
 # python setup.py install
 #
 # prerequisite: setuptools
-
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 REQUIRES = [
     "certifi>=2017.4.17",
